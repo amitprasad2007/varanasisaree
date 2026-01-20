1ef5b7088d8e06c599baebef2b39b507e0c0a558
amit <amit.prasad@learningsprial.co.in>
2026-01-20 23:43:55 +0530

    changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
         $middleware->encryptCookies(except: ['appearance']);
 
         $middleware->web(append: [
