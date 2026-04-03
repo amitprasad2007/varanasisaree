efa75a9b7170b1e66a2d989b2acc60458e2e1d60
amit <amit.prasad@learningsprial.co.in>
2026-04-04 00:27:24 +0530

    changes

---
+++ public/build/assets/dist-DTnGenN7.js
@@ -0,0 +1,1 @@
+import{p as e,s as t}from"./app-mCMCls-R.js";var n=e(t(),1);function r(e){let t=n.useRef({value:e,previous:e});return n.useMemo(()=>(t.current.value!==e&&(t.current.previous=t.current.value,t.current.value=e),t.current.previous),[e])}export{r as t};
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,43 +1,15 @@
 
 import './bootstrap';
 import '../css/app.css';
-import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-
-// Very early diagnostic
-(function () {
-    const el = document.getElementById('app');
-    console.log('[DEBUG] Root element "app":', el ? 'Present' : 'MISSING');
-    if (el) {
-        console.log('[DEBUG] Root element contains data-page:', el.dataset.page ? 'YES' : 'NO');
-        if (el.dataset.page) {
-            try {
-                const page = JSON.parse(el.dataset.page);
-                console.log('[DEBUG] Page name:', page.component);
-                console.log('[DEBUG] Page URL:', page.url);
-            } catch (e) {
-                console.error('[DEBUG] Failed to parse data-page JSON:', e instanceof Error ? e.message : e);
-            }
-        }
-    }
-})();
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => {
-        console.log(`[DEBUG] Resolving component: ${name}`);
-        return resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob<any>('./Pages/**/*.{tsx,jsx}')) as any;
-
-    },
     setup({ el, App, props }) {
-        console.log('[DEBUG] In setup function');
-
-        if (!props || !props.initialPage) {
-            console.error('[DEBUG] NO INITIAL PAGE PROPS IN SETUP');
-        }
+        if (!el) throw new Error('Missing Inertia mount element.');
 
         if (el.hasChildNodes()) {
             hydrateRoot(el, <App {...props} />);
